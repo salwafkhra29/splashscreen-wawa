da0196218228ba7fc22de754cc60d30ad433d560
salwa.imleta@gmail.com <112602374+salwafkhra29@users.noreply.github.com>
2022-11-28 15:10:23 +0700

    first

---
+++ app/src/main/java/com/si5a/splashscreen_wawa/splashActivity.java
@@ -0,0 +1,14 @@
+package com.si5a.splashscreen_wawa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
